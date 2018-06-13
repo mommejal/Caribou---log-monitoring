de3bfd87d06d02b1129b8a4ce06ace494d0fdb20
clafond <corentin.lafond@gmail.com>
2018-06-13 15:37:36 +0200

    test

---
+++ src/main/java/com/caribou/CaribouApplication.java
@@ -0,0 +1,12 @@
+package com.caribou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaribouApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaribouApplication.class, args);
+	}
+}
