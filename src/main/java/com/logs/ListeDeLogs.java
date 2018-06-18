bb8a66e6298e11635fbbde0d54182f071365e53e
mommejal <leonard.mommeja@gmail.com>
2018-06-18 16:10:58 +0200

    ListeDeLogs.java pour avoir une liste a utiliser pour contenir les logs

---
+++ src/main/java/com/logs/ListeDeLogs.java
@@ -0,0 +1,23 @@
+package com.logs;
+
+import java.util.ArrayList;
+import java.util.List;
+public class ListeDeLogs {
+    private List<GeneLog> listeDeLogs;
+
+    public void ListeDeLogsWrapper() {
+         this.listeDeLogs = new ArrayList<GeneLog>();
+    }
+
+    public List<GeneLog> getListeDeLogs() {
+        return listeDeLogs;
+    }
+
+    public void setListeDeLogs(List<GeneLog> listeDeLogs) {
+        this.listeDeLogs = listeDeLogs;
+    }
+
+    public void add(GeneLog log) {
+        this.listeDeLogs.add(log);
+    }
+}
