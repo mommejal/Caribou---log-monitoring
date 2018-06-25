6407f642d9d7e137bcf3ebfb6c36b2070becd7a0
mommejal <leonard.mommeja@gmail.com>
2018-06-25 11:24:17 +0200

    La lecture de la date sous forme de text et l'envoi de parametres marchent

--- src/main/java/com/caribou/LogController.java
+++ src/main/java/com/caribou/LogController.java
@@ -23,71 +23,32 @@ public class LogController {
 	
 	    @Autowired
 		ListeDeLogs logs;
-//        logs.add(new logXMLTest(new String("<![LOG[Exiting Microsoft::EmbeddedDeviceManager::Deployment::UwfManager::CheckWriteFilterProtection...]LOG]!><time=\"13:03:11.840-120\" date=\"06-02-2018\" component=\"MaintenanceCoordinator\" context=\"\" type=\"0\" thread=\"2188\" file=\"uwfmanager.cpp:501\">")));
-//        logs.add(new logXMLTest(new String("<![LOG[Exiting Microsoft::EmbeddedDeviceManager::Deployment::UwfManager::GetVolumeProtectionInfos...]LOG]!><time=\"13:03:11.840-120\" date=\"06-02-2018\" component=\"MaintenanceCoordinator\" context=\"\" type=\"0\" thread=\"2188\" file=\"uwfmanager.cpp:258\">"))); 
-//        logs.add(new logXMLTest(new String("<![LOG[Exiting Microsoft::EmbeddedDeviceManager::Deployment::UwfManager::SuspendProtection...]LOG]!><time=\"13:03:11.855-120\" date=\"06-02-2018\" component=\"MaintenanceCoordinator\" context=\"\" type=\"0\" thread=\"2188\" file=\"uwfmanager.cpp:124\">")));
 	    @Autowired
 	    ModelAndView mav;
-//		ModelAndView mav = new ModelAndView("listeLogs");
-	    public LogDao logdao;
 	    @RequestMapping(method = RequestMethod.GET)
 	    ModelAndView
 	    index()
 	    {
-//	        ModelAndView mav = new ModelAndView("index");
-//	        mav.addObject("hoa_version", "HOA v0.1 parce que je viens de LogController");
 	        mav.setViewName("index");
 	        return mav;
 	    }
-	    
-
-	    @RequestMapping(value="hello",method = RequestMethod.GET)
-	    ModelAndView
-	    hello()
-	    {
-//	        ModelAndView mav = new ModelAndView("hellothymeleaf");
-//	        mav.addAttribute("serverTime", "Il est 10h");
-	        mav.addObject("hello", "hello de la part du Log Controller");
-	    	logXMLTest log = new logXMLTest(new String("<![LOG[Exiting Microsoft::EmbeddedDeviceManager::Deployment::UwfManager::CheckWriteFilterProtection...]LOG]!><time=\"13:03:11.840-120\" date=\"06-02-2018\" component=\"MaintenanceCoordinator\" context=\"\" type=\"0\" thread=\"2188\" file=\"uwfmanager.cpp:501\">"));
-	    	mav.addObject("logMSG" ,log.getMSG());
-	        mav.setViewName("hellothymeleaf");
-	        return mav;
-	    }
-	    	    
-        @GetMapping("/greet")
-        ModelAndView
-        greet(@RequestParam(name="name", required=false, defaultValue="World") String name, ModelAndView model) {
-            model.addObject("name", name);
-            model.setViewName("greet");
-            return model;
-        }
-        
-        @GetMapping("/greeting")
-        ModelAndView
-        greeting(@RequestParam(name="name", required=true, defaultValue="World") String name, ModelAndView model) {
-            model.addObject("name", name);
-            model.setViewName("greeting");
-            return model;
-        }
-//
-	    @RequestMapping(value="/listeLogs",method=RequestMethod.POST)
+	   
+	    @RequestMapping(value="/listeLogs",method=RequestMethod.GET)
 	    @ResponseBody
 	    ModelAndView
-	    listeLogs(ArrayList <GeneLog> logs, ModelAndView mav,@RequestParam(value="filter",required=false, defaultValue="nofilter") String filter) {
+	    listeLogs(ArrayList <GeneLog> logs, ModelAndView mav,@RequestParam(value="filter",required=true/*, defaultValue="nofilter"*/) String filter,@RequestParam(value="datebeginning",required=false) String datebeginning,@RequestParam(value="dateend",required=false) String dateend) {
 	    	// On veut afficher une liste de logs pour l'instant on affiche uniquement les messages
-//	    	ModelAndView mav = new ModelAndView("listeLogs");
-//	    	ArrayList <logXMLTest> logs = new ArrayList<logXMLTest>();
 	        //Quelques logs de test
 	        logs.add(new logXMLTest(new String("<![LOG[Exiting Microsoft::EmbeddedDeviceManager::Deployment::UwfManager::CheckWriteFilterProtection...]LOG]!><time=\"13:03:11.840-120\" date=\"06-02-2018\" component=\"MaintenanceCoordinator\" context=\"\" type=\"0\" thread=\"2188\" file=\"uwfmanager.cpp:501\">")));
 	        logs.add(new logXMLTest(new String("<![LOG[Exiting Microsoft::EmbeddedDeviceManager::Deployment::UwfManager::GetVolumeProtectionInfos...]LOG]!><time=\"13:03:11.840-120\" date=\"06-02-2018\" component=\"MaintenanceCoordinator\" context=\"\" type=\"0\" thread=\"2188\" file=\"uwfmanager.cpp:258\">"))); 
 	        logs.add(new logXMLTest(new String("<![LOG[Exiting Microsoft::EmbeddedDeviceManager::Deployment::UwfManager::SuspendProtection...]LOG]!><time=\"13:03:11.855-120\" date=\"06-02-2018\" component=\"MaintenanceCoordinator\" context=\"\" type=\"0\" thread=\"2188\" file=\"uwfmanager.cpp:124\">")));
 	        //On constate qu'une méthode get sur chaque log suffit à afficher le bon truc
-	        System.out.println("On passe par le liste Logs");
 	        mav.addObject("logs",logs);
 	        mav.addObject("filter",filter);
+	        mav.addObject("datebeginning", datebeginning);
+	        mav.addObject("dateend", dateend);
 	        mav.setViewName("listeLogs");
 	        return mav;
-	        //	        return logdao.findAll();
 	    }
 	    
 //	    @RequestMapping(path="/listeLogs", params={"addRow"},method=RequestMethod.GET)
@@ -114,16 +75,6 @@ public class LogController {
 //	        return "listeLogs";
 //	    }
 	    
-	    @GetMapping(value = "/SeverityLvl/{lvl}")
-	    public String afficherUnLogSeverityLvl(@PathVariable int lvl) {
-	    	return (logdao.findBySeverityLvl(lvl)).toString();
-	    }
-	    
-	    @GetMapping(value = "/AfficherUnLogEtId")
-	    public String afficherUnLogEtId() {
-	    	// Devra prendre en argument un log et l'afficher
-	    	logXMLTest log = new logXMLTest(new String("Contenu du log d'Id="));
-	        return (log.getData()+log.getID());
-	    }	    
+
 	    
 }
