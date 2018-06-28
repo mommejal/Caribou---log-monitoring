b4b881ef8ca5a8ab772eb2cc6ef2bb953d347290
mommejal <leonard.mommeja@gmail.com>
2018-06-28 09:47:39 +0200

    Site marche : Debut Bdd

---
+++ src/main/java/com/bdd/DateLogComparator.java
@@ -0,0 +1,21 @@
+//package com.bdd;
+//import java.util.Comparator;
+//
+//public class DateLogComparator {
+//
+//	    @Override
+//	    public int compare(String x, String y)
+//	    {
+//	        // Assume que c'est un log correcte car il a été séparé
+//	        
+//	        {
+//	            return -1;
+//	        }
+//	        if (x.length() > y.length())
+//	        {
+//	            return 1;
+//	        }
+//	        return 0;
+//	    }
+//	
+//}
--- src/main/java/com/bdd/GestionBdd.java
+++ src/main/java/com/bdd/GestionBdd.java
@@ -1,7 +1,38 @@
-package com.bdd;
-
-import org.json.*;
-
-public class GestionBdd {
-	
-}
+//package com.bdd;
+//
+//import java.io.IOException;
+//import java.util.Queue;
+//
+//import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+//
+//import com.caribou.Logs;
+//import com.fasterxml.jackson.core.JsonGenerationException;
+//import com.fasterxml.jackson.databind.JsonMappingException;
+//import com.fasterxml.jackson.databind.ObjectMapper;
+//
+//
+//public class GestionBdd {
+//
+//	public Queue<String> receptionLog(String jsoninstring) {
+//		//MOI je voudrais recevoir un jsoninstring et le splitter pour obtenir mes logs
+//		ObjectMapper mapper = new ObjectMapper();
+//
+//		try {
+//			// Réception des logs sous forme JSON et renvoie la PriorityQueue voulue
+//			Queue<String> logs = mapper.readValue(jsoninstring, PriorityQueue<String>.class);
+//			return logs;
+//
+//		} catch (JsonGenerationException e) {
+//			e.printStackTrace();
+//		} catch (JsonMappingException e) {
+//			e.printStackTrace();
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
+//		
+//	}
+//	
+//
+//}
+//
+//
