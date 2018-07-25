722de4caba9b0fb8bd133481ece5cd15b9ac8eb3
clafond <corentin.lafond@gmail.com>
2018-07-25 10:16:46 +0200

    merge de sauvetage

--- src/main/java/com/appweb/mavoutput/agents/AgentsMonitor.java
+++ src/main/java/com/appweb/mavoutput/agents/AgentsMonitor.java
@@ -4,7 +4,6 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,9 +11,10 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.agent.paramagent.ParamAgentToManage;
+import com.applications.monitor.AbstractMonitor;
 
 @Controller
-public class AgentsMonitor {
+public class AgentsMonitor extends AbstractMonitor {
 
 	// private static final String DEFAULT_STRING = "RIP_F-ZERO";
 
